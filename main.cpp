2d223e7e546c24071415de8d5e692bf7376454ab
Antosser <71790328+Antosser@users.noreply.github.com>
2021-06-26 16:32:48 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> primes = {};
+	for (unsigned long long i = 2; primes.size() < 1000; i++) {
+		for (int ci : primes) {
+
+		}
+	}
+}
