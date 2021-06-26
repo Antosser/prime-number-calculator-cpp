f18cf107b787fedeebe97f36611581b34998ebbf
Antosser <71790328+Antosser@users.noreply.github.com>
2021-06-26 17:46:29 +0200

    making-it-work

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <string>
+//#include <thread>
+//#include <Windows.h>
 
-int main() {
+int main(int argc, char** argv) {
+	if (argc < 2 || argc > 2)
+		return -1;
+
+	int n = std::stoi(argv[1]);
+
 	std::vector<int> primes = {};
-	for (unsigned long long i = 2; primes.size() < 1000; i++) {
+	for (unsigned long long i = 2; primes.size() < n; i++) {
 		for (int ci : primes) {
-
+			if (i % ci == 0)
+				goto brk;
 		}
+		primes.push_back(i);
+		std::cout << i << std::endl;
+		brk:;
 	}
 }
