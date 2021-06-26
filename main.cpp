4eb39462c87a18f4e266e5f7e77f318f39baebd4
Antosser <71790328+Antosser@users.noreply.github.com>
2021-06-26 20:18:24 +0200

    working

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <string>
 //#include <thread>
 //#include <Windows.h>
 
 int main(int argc, char** argv) {
-	if (argc < 2 || argc > 2)
+	if (argc != 3) {
+		std::cout << "Expected 2 arguments\n";
 		return -1;
+	}
 
 	int n = std::stoi(argv[1]);
+	std::ofstream file(argv[2]);
+	std::cout << "Numbers: " << n << std::endl;
 
 	std::vector<int> primes = {};
 	for (unsigned long long i = 2; primes.size() < n; i++) {
 		for (int ci : primes) {
+			if (i % 10000 == 0)
+				std::cout << primes.size() << std::endl;
 			if (i % ci == 0)
 				goto brk;
 		}
 		primes.push_back(i);
-		std::cout << i << std::endl;
+		file << i << std::endl;
 		brk:;
 	}
+	file.close();
 }
